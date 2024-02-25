9539f7301b87709eb454c3d2276a27d3ce92fbc6
proggodd <ayhaga.hesa@gmail.com>
2024-02-25 21:49:00 +0200

    adding mapping profile

---
+++ AdControllerApi/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AdControllerApi.Dtos;
+using AutoMapper;
+using MyAdsApi.Models;
+
+namespace MyAdsApi.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserLoginDto, User>(); // Mapping from UserLoginDto to User
+            CreateMap<RewardClaimRequestDto, RewardClaimRequest>(); // Mapping from RewardClaimRequestDto to RewardClaimRequest
+            CreateMap<AdSettingUpdateRequestDto, AdSetting>(); // Mapping from AdSettingUpdateRequestDto to AdSetting
+            // Add more mappings as needed
+        }
+    }
+}
